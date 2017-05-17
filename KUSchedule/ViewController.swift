5989f5c82c59f78f97786ec0d9144a50521fd20b
Varis <win3455@hotmail.com>
2017-05-17 17:05:44 +0700

    Initial Commit

---
+++ KUSchedule/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KUSchedule
+//
+//  Created by Varis Kritpolchai on 5/17/2560 BE.
+//  Copyright © 2560 Varis Kritpolchai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ KUScheduleTests/KUScheduleTests.swift
@@ -0,0 +1,36 @@
+//
+//  KUScheduleTests.swift
+//  KUScheduleTests
+//
+//  Created by Varis Kritpolchai on 5/17/2560 BE.
+//  Copyright © 2560 Varis Kritpolchai. All rights reserved.
+//
+
+import XCTest
+@testable import KUSchedule
+
+class KUScheduleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
