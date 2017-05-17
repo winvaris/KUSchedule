b7cd805c95a82a89becbc0966d3aa90908b2e535
Varis <win3455@hotmail.com>
2017-05-18 02:32:00 +0700

    First Commit

---
+++ StudentIDViewController.swift
@@ -0,0 +1,44 @@
+//
+//  StudentIDViewController.swift
+//  KUSchedule
+//
+//  Created by Varis Kritpolchai on 5/18/2560 BE.
+//  Copyright © 2560 Varis Kritpolchai. All rights reserved.
+//
+
+import UIKit
+
+class StudentIDViewController: UIViewController {
+
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let value = UIInterfaceOrientation.portrait.rawValue
+        UIDevice.current.setValue(value, forKey: "orientation")
+        self.hideKeyboardWhenTappedAround()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
